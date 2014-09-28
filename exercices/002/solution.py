2754ea00c0e3cf46f2f8335cb5d1169335d60d10
Dan Harbaugh <dan-harbaugh@outlook.com>
2014-09-28 15:57:27 -0700

    solution for 002

---
+++ exercices/002/solution.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Exercise 002
+"""
+import sys
+argument=sys.argv[1]
+
+def reverse_string(string):
+  length = len(string)
+  reverse_string=''
+  while length>0:
+    character=string[length - 1:length]
+    if character in 'aeiou':
+      character = character.upper()
+    reverse_string=reverse_string + character
+    length = length - 1
+  return reverse_string
+
+print (reverse_string(sys.argv[1]))
