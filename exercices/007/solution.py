1f9beed5d5e471a6c859f75bd0dd8d07dcf2db10
Dan Harbaugh <dan-harbaugh@outlook.com>
2014-09-28 16:18:16 -0700

    solution for 007

---
+++ exercices/007/solution.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Exercise 007
+"""
+
+#import sys
+
+#print(sys.argv[0])
+
+def print_cascade(string):
+  for character in string:
+    print(character)
+
+print_cascade("Hello World !")
