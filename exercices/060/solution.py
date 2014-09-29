ff56e6f5910b291cd70019e39994f4fd2407bcab
Dan Harbaugh <dan-harbaugh@outlook.com>
2014-09-28 17:13:21 -0700

    solution 060

---
+++ exercices/060/solution.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Exercise 060
+"""
+
+alphabet="abcdefghijklmnopqrstuvwxyz"
+
+for char in alphabet:
+  for paired in alphabet:
+    print(char,end="")
+    print(paired)
